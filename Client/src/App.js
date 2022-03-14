a243c2bd78bd18c0aab00671d51d3bb014e33bbb
m-k-fe <fsd_hack@hotmail.com>
2022-03-14 02:42:45 +0000

    filter-component

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,13 +6,45 @@ import Products from "./Components/Products/Products";
 import data from "./data.json";
 function App() {
   const [products , setProducts] = useState(data);
+  const [sort , setSort] = useState("");
+  const [size , setSize] = useState("");
+  const handleFilterBySize = (e) => {
+    setSize(e.target.value);
+    if (e.target.value == "all"){
+      setProducts(data);
+    }else{
+      let cloneProducts = [...data];
+      let filtredProducts = cloneProducts.filter(item => item.sizes.indexOf(e.target.value) !== -1);
+      setProducts(filtredProducts);
+    }
+  };
+  const handleFilterBySort = (e) => {
+    setSort(e.target.value);
+    let cloneProducts = [...data];
+    let newProducts = cloneProducts.sort((a , b) => {
+      if (e.target.value === "lowest"){
+        return a.price - b.price;
+      }else if (e.target.value === "highest"){
+        return b.price - a.price;
+      }else{
+        return a.id < b.id ? 1 : -1;
+      };    
+    });
+    setProducts(newProducts);
+  };
+
   return (
     <div className="layout">
       <Header />
       <main>
         <div className="wrapper">
           <Products products={products} />
-          <Filter />
+          <Filter 
+            size={size}
+            sort={sort}
+            handleFilterBySize={handleFilterBySize}
+            handleFilterBySort={handleFilterBySort}
+          />
         </div>
       </main>
       <Footer />
--- Client/src/Components/Filter/Filter.js
+++ Client/src/Components/Filter/Filter.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import "../../css/Filter/Filter.css";
 function Filter(props) {
+    const {size , sort , handleFilterBySize , handleFilterBySort} = props;
     return (
         <div className="filter-wrapper">
             <h2 className="filter-wrapper-title">Filter</h2>
             <div className="num-of-products">Number Of Products : 4</div>
             <div className="filter-by-size">
                 <span>Filter</span>
-                <select className="filter-select">
+                <select value={size} onChange={handleFilterBySize} className="filter-select">
                     <option value="all">ALL</option>
                     <option value="xs">XS</option>
                     <option value="s">S</option>
@@ -19,9 +20,9 @@ function Filter(props) {
             </div>
             <div className="filter-by-size">
                 <span>Order</span>
-                <select className="filter-select">
-                    <option value="lastest">Lastest</option>
-                    <option value="lowter">Lowter</option>
+                <select value={sort} onChange={handleFilterBySort} className="filter-select">
+                    <option value="latest">Latest</option>
+                    <option value="lowest">Lowest</option>
                     <option value="highest">Highest</option>
                 </select>
             </div>
